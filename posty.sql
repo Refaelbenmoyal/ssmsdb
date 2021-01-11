9abca77953b67d9e3d02e4f909554af136732189
refaelbenmoyal <67203489+Refaelbenmoyal@users.noreply.github.com>
2021-01-11 21:56:06 +0200

    Add files via upload

---
+++ posty.sql
@@ -0,0 +1,646 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.1
+-- Dumped by pg_dump version 13.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: pgagent; Type: SCHEMA; Schema: -; Owner: postgres
+--
+
+CREATE SCHEMA pgagent;
+
+
+ALTER SCHEMA pgagent OWNER TO postgres;
+
+--
+-- Name: SCHEMA pgagent; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON SCHEMA pgagent IS 'pgAgent system tables';
+
+
+--
+-- Name: adminpack; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS adminpack WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION adminpack; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION adminpack IS 'administrative functions for PostgreSQL';
+
+
+--
+-- Name: pgagent; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pgagent WITH SCHEMA pgagent;
+
+
+--
+-- Name: EXTENSION pgagent; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION pgagent IS 'A PostgreSQL job scheduler';
+
+
+--
+-- Name: all_workers_on_a_particular_construction_site(bigint, text, text); Type: PROCEDURE; Schema: public; Owner: postgres
+--
+
+CREATE PROCEDURE public.all_workers_on_a_particular_construction_site(_id bigint, _name text, _address text)
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+        select * from sites where address = haifa;
+
+    END;
+    $$;
+
+
+ALTER PROCEDURE public.all_workers_on_a_particular_construction_site(_id bigint, _name text, _address text) OWNER TO postgres;
+
+--
+-- Name: salary_workers(integer); Type: PROCEDURE; Schema: public; Owner: postgres
+--
+
+CREATE PROCEDURE public.salary_workers(x integer)
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+        IF(X=1)
+            THEN
+            UPDATE  workers SET salary = 20000
+            where workers.roles_id = x;
+            Else
+            update workers set  salary = (random()*5000)+5000
+            where roles_id>x;
+        end if;
+
+    END;
+    $$;
+
+
+ALTER PROCEDURE public.salary_workers(x integer) OWNER TO postgres;
+
+--
+-- Name: select_workers_and_roles_name(bigint, text, integer); Type: PROCEDURE; Schema: public; Owner: postgres
+--
+
+CREATE PROCEDURE public.select_workers_and_roles_name(_id bigint, _name text, _phone integer)
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+        select * from workers;
+         select * from roles;
+    END;
+    $$;
+
+
+ALTER PROCEDURE public.select_workers_and_roles_name(_id bigint, _name text, _phone integer) OWNER TO postgres;
+
+--
+-- Name: site_with_the_most_workers(bigint, text); Type: PROCEDURE; Schema: public; Owner: postgres
+--
+
+CREATE PROCEDURE public.site_with_the_most_workers(_id bigint, _name text)
+    LANGUAGE plpgsql
+    AS $$
+    BEGIN
+        select * from sites where id = 1;
+
+    END;
+    $$;
+
+
+ALTER PROCEDURE public.site_with_the_most_workers(_id bigint, _name text) OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: actors; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.actors (
+    id bigint NOT NULL,
+    name text,
+    birthdate date
+);
+
+
+ALTER TABLE public.actors OWNER TO postgres;
+
+--
+-- Name: actors_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.actors_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.actors_id_seq OWNER TO postgres;
+
+--
+-- Name: actors_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.actors_id_seq OWNED BY public.actors.id;
+
+
+--
+-- Name: genres; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.genres (
+    id bigint NOT NULL,
+    name text
+);
+
+
+ALTER TABLE public.genres OWNER TO postgres;
+
+--
+-- Name: genres_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.genres_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.genres_id_seq OWNER TO postgres;
+
+--
+-- Name: genres_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.genres_id_seq OWNED BY public.genres.id;
+
+
+--
+-- Name: movies; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.movies (
+    id bigint NOT NULL,
+    name text,
+    "release date" date,
+    genre_id bigint
+);
+
+
+ALTER TABLE public.movies OWNER TO postgres;
+
+--
+-- Name: movies_actor; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.movies_actor (
+    id bigint NOT NULL,
+    movies_id bigint,
+    actor_id bigint
+);
+
+
+ALTER TABLE public.movies_actor OWNER TO postgres;
+
+--
+-- Name: movies_actor_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.movies_actor_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.movies_actor_id_seq OWNER TO postgres;
+
+--
+-- Name: movies_actor_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.movies_actor_id_seq OWNED BY public.movies_actor.id;
+
+
+--
+-- Name: movies_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.movies_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.movies_id_seq OWNER TO postgres;
+
+--
+-- Name: movies_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.movies_id_seq OWNED BY public.movies.id;
+
+
+--
+-- Name: roles; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.roles (
+    id bigint NOT NULL,
+    name text
+);
+
+
+ALTER TABLE public.roles OWNER TO postgres;
+
+--
+-- Name: sites; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.sites (
+    id bigint NOT NULL,
+    name text,
+    address text
+);
+
+
+ALTER TABLE public.sites OWNER TO postgres;
+
+--
+-- Name: workers; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.workers (
+    id bigint NOT NULL,
+    name text,
+    phone integer,
+    salary integer,
+    role_id bigint,
+    site_id bigint
+);
+
+
+ALTER TABLE public.workers OWNER TO postgres;
+
+--
+-- Name: workers_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.workers_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.workers_id_seq OWNER TO postgres;
+
+--
+-- Name: workers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.workers_id_seq OWNED BY public.workers.id;
+
+
+--
+-- Name: actors id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.actors ALTER COLUMN id SET DEFAULT nextval('public.actors_id_seq'::regclass);
+
+
+--
+-- Name: genres id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.genres ALTER COLUMN id SET DEFAULT nextval('public.genres_id_seq'::regclass);
+
+
+--
+-- Name: movies id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies ALTER COLUMN id SET DEFAULT nextval('public.movies_id_seq'::regclass);
+
+
+--
+-- Name: movies_actor id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies_actor ALTER COLUMN id SET DEFAULT nextval('public.movies_actor_id_seq'::regclass);
+
+
+--
+-- Name: workers id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.workers ALTER COLUMN id SET DEFAULT nextval('public.workers_id_seq'::regclass);
+
+
+--
+-- Data for Name: pga_jobagent; Type: TABLE DATA; Schema: pgagent; Owner: postgres
+--
+
+COPY pgagent.pga_jobagent (jagpid, jaglogintime, jagstation) FROM stdin;
+4992	2021-01-09 18:42:21.020981+00	LAPTOP-L2HJCOPI
+\.
+
+
+--
+-- Data for Name: pga_jobclass; Type: TABLE DATA; Schema: pgagent; Owner: postgres
+--
+
+COPY pgagent.pga_jobclass (jclid, jclname) FROM stdin;
+\.
+
+
+--
+-- Data for Name: pga_job; Type: TABLE DATA; Schema: pgagent; Owner: postgres
+--
+
+COPY pgagent.pga_job (jobid, jobjclid, jobname, jobdesc, jobhostagent, jobenabled, jobcreated, jobchanged, jobagentid, jobnextrun, joblastrun) FROM stdin;
+\.
+
+
+--
+-- Data for Name: pga_schedule; Type: TABLE DATA; Schema: pgagent; Owner: postgres
+--
+
+COPY pgagent.pga_schedule (jscid, jscjobid, jscname, jscdesc, jscenabled, jscstart, jscend, jscminutes, jschours, jscweekdays, jscmonthdays, jscmonths) FROM stdin;
+\.
+
+
+--
+-- Data for Name: pga_exception; Type: TABLE DATA; Schema: pgagent; Owner: postgres
+--
+
+COPY pgagent.pga_exception (jexid, jexscid, jexdate, jextime) FROM stdin;
+\.
+
+
+--
+-- Data for Name: pga_joblog; Type: TABLE DATA; Schema: pgagent; Owner: postgres
+--
+
+COPY pgagent.pga_joblog (jlgid, jlgjobid, jlgstatus, jlgstart, jlgduration) FROM stdin;
+\.
+
+
+--
+-- Data for Name: pga_jobstep; Type: TABLE DATA; Schema: pgagent; Owner: postgres
+--
+
+COPY pgagent.pga_jobstep (jstid, jstjobid, jstname, jstdesc, jstenabled, jstkind, jstcode, jstconnstr, jstdbname, jstonerror, jscnextrun) FROM stdin;
+\.
+
+
+--
+-- Data for Name: pga_jobsteplog; Type: TABLE DATA; Schema: pgagent; Owner: postgres
+--
+
+COPY pgagent.pga_jobsteplog (jslid, jsljlgid, jsljstid, jslstatus, jslresult, jslstart, jslduration, jsloutput) FROM stdin;
+\.
+
+
+--
+-- Data for Name: actors; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.actors (id, name, birthdate) FROM stdin;
+1	dani	1980-12-20
+2	rami	1985-10-05
+\.
+
+
+--
+-- Data for Name: genres; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.genres (id, name) FROM stdin;
+1	drama
+2	action
+\.
+
+
+--
+-- Data for Name: movies; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.movies (id, name, "release date", genre_id) FROM stdin;
+1	superman	2000-10-20	\N
+2	lion king	1990-10-20	\N
+\.
+
+
+--
+-- Data for Name: movies_actor; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.movies_actor (id, movies_id, actor_id) FROM stdin;
+\.
+
+
+--
+-- Data for Name: roles; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.roles (id, name) FROM stdin;
+1	constructor
+2	architect
+\.
+
+
+--
+-- Data for Name: sites; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.sites (id, name, address) FROM stdin;
+1	north	haifa
+2	south	eilat
+\.
+
+
+--
+-- Data for Name: workers; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.workers (id, name, phone, salary, role_id, site_id) FROM stdin;
+1	rami	505214785	\N	\N	\N
+2	dani	521245875	\N	\N	\N
+3	avi	548542187	\N	\N	\N
+\.
+
+
+--
+-- Name: actors_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.actors_id_seq', 1, false);
+
+
+--
+-- Name: genres_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.genres_id_seq', 1, false);
+
+
+--
+-- Name: movies_actor_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.movies_actor_id_seq', 1, false);
+
+
+--
+-- Name: movies_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.movies_id_seq', 1, false);
+
+
+--
+-- Name: workers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.workers_id_seq', 1, false);
+
+
+--
+-- Name: actors actors_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.actors
+    ADD CONSTRAINT actors_pk PRIMARY KEY (id);
+
+
+--
+-- Name: genres genres_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.genres
+    ADD CONSTRAINT genres_pk PRIMARY KEY (id);
+
+
+--
+-- Name: movies_actor movies_actor_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies_actor
+    ADD CONSTRAINT movies_actor_pk PRIMARY KEY (id);
+
+
+--
+-- Name: movies movies_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies
+    ADD CONSTRAINT movies_pk PRIMARY KEY (id);
+
+
+--
+-- Name: roles roles_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.roles
+    ADD CONSTRAINT roles_pk PRIMARY KEY (id);
+
+
+--
+-- Name: sites sites_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.sites
+    ADD CONSTRAINT sites_pk PRIMARY KEY (id);
+
+
+--
+-- Name: workers workers_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.workers
+    ADD CONSTRAINT workers_pk PRIMARY KEY (id);
+
+
+--
+-- Name: workers_role_id_index; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX workers_role_id_index ON public.workers USING btree (role_id);
+
+
+--
+-- Name: movies_actor movies_actor_actors_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies_actor
+    ADD CONSTRAINT movies_actor_actors_id_fk FOREIGN KEY (actor_id) REFERENCES public.actors(id);
+
+
+--
+-- Name: movies_actor movies_actor_movies_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies_actor
+    ADD CONSTRAINT movies_actor_movies_id_fk FOREIGN KEY (movies_id) REFERENCES public.movies(id);
+
+
+--
+-- Name: movies movies_genres_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.movies
+    ADD CONSTRAINT movies_genres_id_fk FOREIGN KEY (genre_id) REFERENCES public.genres(id);
+
+
+--
+-- Name: workers workers_roles_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.workers
+    ADD CONSTRAINT workers_roles_id_fk FOREIGN KEY (role_id) REFERENCES public.roles(id);
+
+
+--
+-- Name: workers workers_sites_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.workers
+    ADD CONSTRAINT workers_sites_id_fk FOREIGN KEY (site_id) REFERENCES public.sites(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
